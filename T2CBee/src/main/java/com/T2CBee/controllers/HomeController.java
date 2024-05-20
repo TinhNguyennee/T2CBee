883891af331da4f36e431567af1d78c4b45c6b45
Tinh Nguyen <tinhps28445@fpt.edu.vn>
2024-05-20 18:17:28 +0700

    hehe loi~ toi

--- T2CBee/src/main/java/com/T2CBee/controllers/HomeController.java
+++ T2CBee/src/main/java/com/T2CBee/controllers/HomeController.java
@@ -1,26 +1,19 @@
 package com.T2CBee.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 
 
 @Controller
 public class HomeController {
 	@GetMapping("home/index")
-	public String home(@PathVariable("path") String path, Model model) {
-	    model.addAttribute("path", path);
+	public String home() {
+
 	    return "home/index";
 	}
 
-	@RequestMapping("{x}")
-	public String requestMethodName(@PathVariable("x") String x, Model model) {
-	    model.addAttribute("path", x);
-	    return "home/index";
-	}
+
 
 
 
